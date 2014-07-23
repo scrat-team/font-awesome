4af97121559fd67ceb7920607b2e3fd46470db2c
Hinc Liu <hincubus@gmail.com>
2014-07-23 16:54:39 +0800

    加入 travis 自动转换为可安装组件

---
+++ README.md
@@ -0,0 +1,4 @@
+font-awesome
+============
+
+transform [FortAwesome/Font-Awesome](https://github.com/FortAwesome/Font-Awesome) into installable [scrat-component](https://github.com/scrat-team) with [Travis](https://travis-ci.org/)
---
+++ transformer/config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  orgiRepo: 'FortAwesome/Font-Awesome',
+  ref: 'master',
+  target: __dirname,
+  name: 'font-awesome',
+  repo: 'scrat-team/font-awesome',
+  description: 'a scrat-installable fork of FortAwesome/Font-Awesome',
+  main: {
+    'css/font-awesome.css': 'css/font-awesome.css'
+  },
+  fonts: {
+    'fonts/fontawesome-webfont.eot': 'fonts/fontawesome-webfont.eot',
+    'fonts/fontawesome-webfont.svg': 'fonts/fontawesome-webfont.svg',
+    'fonts/fontawesome-webfont.ttf': 'fonts/fontawesome-webfont.ttf',
+    'fonts/fontawesome-webfont.woff': 'fonts/fontawesome-webfont.woff',
+    'fonts/FontAwesome.otf': 'fonts/FontAwesome.otf'
+  }
+};
